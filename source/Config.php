e5172034be109e3a1da963060a437cbc4f97fb56
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-05-03 13:33:17 +0200

    Google auth

--- source/Config.php
+++ source/Config.php
@@ -3,7 +3,7 @@
 define("GOOGLE", [
   'clientId'     => '994783721724-mf5s94s74754btp64ctf99gj8ilfgppf.apps.googleusercontent.com',
   'clientSecret' => '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',
-  'redirectUri'  => 'http://localhost/projects/arny_mensa/mensaaci_php/auth/'
+  'redirectUri'  => 'http://mensa.marceloschneider.dev.br/auth/'
 ]);
 define("URI", [
   'dashboard'  => '../calendario',
